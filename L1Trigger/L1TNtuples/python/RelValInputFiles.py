03465a350db705e1daa2a82763a9ec474ec2f0e6
Dinyar Rabady <dinyar@rabady.at>
2015-08-16 19:09:23 +0200

    Synchronized RelVals with targeted CMSSW release.

--- L1Trigger/L1TNtuples/python/RelValInputFiles.py
+++ L1Trigger/L1TNtuples/python/RelValInputFiles.py
@@ -3,7 +3,7 @@
 
 # AOD
 def RelValInputFile_AOD():
-    return '/store/relval/CMSSW_7_5_0_pre1/RelValProdTTbar_13/AODSIM/MCRUN2_74_V7-v1/00000/48159643-5EE3-E411-818F-0025905A48F0.root'
+    return '/store/relval/CMSSW_7_4_8_patch1/RelValProdTTbar_13/AODSIM/MCRUN2_74_V11_mulTrh-v1/00000/0A41D4BF-523C-E511-A8E1-0026189438FD.root'
 
 # DIGI (only available in RelVal, not a realistic workflow)
 def RelValInputFile_DIGI():
@@ -11,7 +11,7 @@ def RelValInputFile_DIGI():
 
 # RAW (need to run RawToDigi to use this)
 def RelValInputFile_RAW():
-    return '/store/relval/CMSSW_7_5_0_pre4/RelValProdTTbar_13/GEN-SIM-RAW/MCRUN2_75_V1-v1/00000/1CFADAF5-E1F5-E411-A406-0025905A60D6.root'
+    return '/store/relval/CMSSW_7_4_8_patch1/RelValProdTTbar_13/GEN-SIM-RAW/MCRUN2_74_V11_mulTrh-v1/00000/2AEA6F95-4A3C-E511-8B2F-0025905A48FC.root'
 
 #'/store/relval/CMSSW_7_5_0_pre1/RelValProdTTbar_13/GEN-SIM-RAW/MCRUN2_74_V7-v1/00000/0CEB1526-6CE3-E411-82B6-00261894386C.root'
 
