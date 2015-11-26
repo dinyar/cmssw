d1d20995d5d4ad7525a67f860fe993724c007084
Thomas Reis <thomas.reis@cern.ch>
2015-11-26 11:01:45 +0100

    changes to run muon emulator from GEN-SIM-RAW

--- L1Trigger/L1TMuon/test/runMuon.py
+++ L1Trigger/L1TMuon/test/runMuon.py
@@ -10,10 +10,11 @@ process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(50)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
 process.source = cms.Source('PoolSource',
- fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_10k_eta1.root')
+ #fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_10k_eta1.root')
+ fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/t/treis/public/testsamples/DYToMuMu_M-50_Tune4C_13TeV-pythia8_PU20bx25_tsg_castor_PHYS14_25_V1-v1_GEN-SIM-RAW_10000_044B58B4-9D75-E411-AB6C-002590A83218.root')
 	                    )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(50))
 
 # PostLS1 geometry used
 process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
@@ -52,6 +53,26 @@ process.load('L1Trigger.L1TCalorimeter.caloStage2Layer1Digis_cfi')
 process.caloStage2Layer1Digis.ecalToken = cms.InputTag("simEcalTriggerPrimitiveDigis")
 process.caloStage2Layer1Digis.hcalToken = cms.InputTag("simHcalTriggerPrimitiveDigis")
 
+# changes to run in PHYS14 GEN-SIM-RAW
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+process.caloStage2Layer1Digis.ecalToken = cms.InputTag("ecalDigis", "EcalTriggerPrimitives")
+process.caloStage2Layer1Digis.hcalToken = cms.InputTag("hcalDigis")
+process.simEmtfDigis.CSCInput = cms.InputTag("csctfDigis")
+process.simOmtfDigis.srcDTPh = cms.InputTag('dttfDigis')
+process.simOmtfDigis.srcDTTh = cms.InputTag('dttfDigis')
+process.simOmtfDigis.srcCSC = cms.InputTag('csctfDigis')
+process.simOmtfDigis.srcRPC = cms.InputTag('muonRPCDigis')
+process.simTwinMuxDigis.DTDigi_Source = cms.InputTag("dttfDigis")
+process.simTwinMuxDigis.DTThetaDigi_Source = cms.InputTag("dttfDigis")
+process.simTwinMuxDigis.RPC_Source = cms.InputTag("muonRPCDigis")
+process.simBmtfDigis.DTDigi_Theta_Source = cms.InputTag("dttfDigis")
+
+process.L1ReEmulSeq = cms.Sequence(  process.ecalDigis
+                                   + process.hcalDigis
+                                   + process.csctfDigis
+                                   + process.dttfDigis
+                                   + process.muonRPCDigis
+                                  )
 process.L1TMuonSeq = cms.Sequence(   process.caloStage2Layer1Digis
                                    + process.simTwinMuxDigis
                                    + process.simBmtfDigis 
@@ -64,7 +85,7 @@ process.L1TMuonSeq = cms.Sequence(   process.caloStage2Layer1Digis
 #                                   + process.l1tSummary
 )
 
-process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
+process.L1TMuonPath = cms.Path(process.L1ReEmulSeq + process.L1TMuonSeq)
 
 process.out = cms.OutputModule("PoolOutputModule", 
    fileName = cms.untracked.string("l1tbmtf_superprimitives1.root")
